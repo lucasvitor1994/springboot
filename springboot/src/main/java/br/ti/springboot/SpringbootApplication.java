ef317f8cf193e04d9f4a05badddb295adfb2cffe
lucasvitor1994 <55961366+lucasvitor1994@users.noreply.github.com>
2023-03-02 12:08:06 -0300

    homolog branch-homolog

--- springboot/src/main/java/br/ti/springboot/SpringbootApplication.java
+++ springboot/src/main/java/br/ti/springboot/SpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
-
+//testando branch
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
